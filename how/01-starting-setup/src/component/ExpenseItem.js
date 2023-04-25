c28baf2d4054e872ca7ee2b9c6eedea6de8995d4
Abhijeet <learnerrajhans@gmail.com>
2023-04-25 02:28:22 -0700

    src/component/ExpenseItem.js

---
+++ how/01-starting-setup/src/component/ExpenseItem.js
@@ -0,0 +1,15 @@
+import './ExpenseItem.css';
+
+function ExpenseItem(){
+  return (<div>
+    <>
+      <div className="expense-item">April 20 2023</div>
+      <div className="expense-item__description">
+        <h2 className="expense-item h2">Books</h2>
+        </div>
+      <div className="expense-item__price">$450.50</div>
+    </>
+    </div>);
+  
+}
+export default ExpenseItem;
